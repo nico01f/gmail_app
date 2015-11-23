4a71818ee6c71a349fe07468b18b4acd12833cf5
Nico Fuenzalida <nico01f@gmail.com>
2015-11-23 15:53:03 -0300

    Moved connection object like a global variable

--- gmail.rb
+++ gmail.rb
@@ -1,31 +1,32 @@
 require 'gmail'
-require 'colorize' 
+require 'colorize'
 require 'yaml'
 
+ACCOUNT = YAML.load_file('account.yml') unless defined? ACCOUNT
 
-ACCOUNT = YAML.load_file("account.yml") unless defined? ACCOUNT
+G = Gmail.connect(ACCOUNT['email'], ACCOUNT['password'])
 
-#Conecta con Gmail
-def conecta(email, pass)
-	g = Gmail.connect(email, pass)
-	if g.logged_in? ==  true
-		puts "Connected!".green
-	else
-		puts "Doh! Not connected".red
-	end
+def unread
+  n = G.inbox.count(:unread)
+  puts "There #{n} mails unread."
 end
 
-
-def info_credencial()
-	puts "Usuario: #{ACCOUNT['email']}"
-        password_hided = "*" * ACCOUNT['password'].length
-	puts "Password: #{password_hided}"
-	puts "============="
-	conecta(ACCOUNT['email'], ACCOUNT['password'])
+def connected
+  if G.logged_in? == true
+    puts 'Connected!'.green
+  else
+    puts 'Doh! Not connected'.red
+  end
 end
 
-info_credencial()
-puts "Exiting...".yellow
+def info_credencial
+  puts "Usuario: #{ACCOUNT['email']}"
+  password_hided = '*' * ACCOUNT['password'].length
+  puts "Password: #{password_hided}"
+  puts '==============='
+  connected
+  unread
+end
 
-
-
+info_credencial
+puts 'Exiting...'.yellow
